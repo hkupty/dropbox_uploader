c305889e28251e1d8b541ef322860c90448bcd30
Henry Kupty <hkupty@gmail.com>
2014-04-13 17:16:46 -0300

    Add setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='drobpx_upload',
+    version='0.1.1',
+    author='Henry "Ingvij" Kupty',
+    author_email='hkupty@gmail.com',
+    packages=['dropbox_uploader'],
+    url='https://github.com/hkupty/dropbox_uploader',
+    license='LICENSE.txt',
+    description='Fairly simple dropbox backup script written in python',
+    install_requires=[
+        "mechanize >= 0.2.5",
+    ],
+)
