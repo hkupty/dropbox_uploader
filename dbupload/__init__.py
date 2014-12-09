41a92df9a21fd3f85b9cd23fb6c9751d0a86a6bf
Henry Kupty <hkupty@gmail.com>
2014-12-09 10:09:52 -0200

    Fix __init__.py

--- dbupload/__init__.py
+++ dbupload/__init__.py
@@ -1,6 +1,12 @@
-import dbupload
+# encoding: utf-8
+""" PyDropbox is a fork of dropbox_upload.
 
-from upload import upload_file
-from dbconn import DropboxConnection
+The aim of this project is to supply a stable dropbox connection,
+allowing scriptable upload of documents.
+"""
+from dbupload.upload import upload_file
+from dbupload.dbconn import DropboxConnection
 
-__all__ = ['upload_file','DropboxConnection']
+__all__ = [
+    'upload_file', 'DropboxConnection'
+]
